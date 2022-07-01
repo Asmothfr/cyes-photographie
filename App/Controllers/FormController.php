9ff431daef468502602412eedb04d0c7fbad893b
Asmothfr <yoan.sabatier@gmail.com>
2022-07-01 17:38:59 +0200

    form error failed fixing

--- App/Controllers/FormController.php
+++ App/Controllers/FormController.php
@@ -9,31 +9,59 @@ class Formcontroller extends LayoutController
 {
     public function testFormValidation()
     {
-        try
+        // if( isset($_POST["test1"]) && !empty($_POST["test1"]) &&
+        //     isset($_POST["test2"]) && !empty($_POST["test2"]))
+        // {
+        //     $data = [
+        //         "d1"=>$_POST["test1"],
+        //         "d2"=>$_POST["test2"],
+        //     ];
+        //     $model = new TestModel();
+        //     $model->contactFormValidation($data);
+        //     $this->render("test_formulaire");
+        // }
+        // else
+        // {
+        //     $error= "Veuillez remplir tous les champs.";
+        //     $this->render("test_formulaire", ["error"=>$error]);
+        // }
+
+        // $data = [];
+        $errors = [];
+
+        if(isset($_POST["test1"]) && !empty($_POST["test1"]))
         {
-            if( isset($_POST["test1"]) && !empty($_POST["test1"]) &&
-                isset($_POST["test2"]) && !empty($_POST["test2"]))
-            {
-                $data = [
-                    "d1"=>$_POST["test1"],
-                    "d2"=>$_POST["test2"],
-                ];
-                $model = new TestModel();
-                $model->contactFormValidation($data);
-                header( 'Location: index.php?route=test' );
-                
-            }
-            else
-            {
-                throw new \Exception("Veuillez remplir tous les champs.");
-            }
-            
+            array_push($data, $_POST["test1"]);
         }
-        catch( \Exception $e )
+        else
         {
-            $errorMessage = $e->getMessage();
-            header('Location: index.php?route=test&errorMessage=' . $errorMessage);
-            exit();
+            // $error1 = "Veuillez remplir champs 1.";
+            array_push($errors,  $error1 = "Veuillez remplir champs 1.");
+        }
+        if(!isset($_POST["test2"]) && empty($_POST["test2"]))
+        {
+            array_push($data, $_POST["test2"]);
+        }
+        else
+        {
+            $error2 = "Veuillez remplir champs 2.";
+            array_push($errors, $error2);
+        }
+        //je vérifie dans mon tableau d'erreur.
+        // if(isset($error1) && !empty($error1))
+        if(isset($errors)&&!empty($errors))
+        {
+            $this->render("test_formulaire",["errors"=>$errors]);
+        }
+        else
+        {
+            $data = [
+                        "d1"=>$_POST["test1"],
+                        "d2"=>$_POST["test2"],
+                    ];
+                    $model = new TestModel();
+                    $model->contactFormValidation($data);
+                    $this->render("test_formulaire");
         }
     }
 }
