4e7455300625131417f85a2d886ab918bb18f580
Asmothfr <yoan.sabatier@gmail.com>
2022-07-26 17:16:27 +0200

    admin info clean

--- 654381dfhgf1871t2gdf03/App/Controllers/UpdateController.php
+++ 654381dfhgf1871t2gdf03/App/Controllers/UpdateController.php
@@ -9,10 +9,6 @@ class UpdateController extends LayoutController
 {
     public function AdminInfoUpdate()
     {
-        // print_r($_POST);
-        // echo("<br>");
-        // print_r($id);
-        // // die;
         $errors = [];
         $id = $_GET["adm_id"];
         $model = new AdminModel;
@@ -28,8 +24,6 @@ class UpdateController extends LayoutController
                 }
                 else
                 {
-                    echo("ERREUR 1");
-                    echo("<br>");
                     $errors["login"] = "Veuillez remplir le champs login avant de valider.";
                 }
                 break;
@@ -44,16 +38,12 @@ class UpdateController extends LayoutController
                 }
                 else
                 {
-                    echo("ERREUR 2");
-                    echo("<br>");
                     $errors["password"] = "Les mots de passe ne sont pas identiques";
                 }
                 break;
             case isset($_POST["lastname"]):
                 if(!empty($_POST["lastname"]))
                 {
-                    echo("OK 3");
-                    echo("<br>");
                     $column = "adm_last_name";
                     $data = $_POST["lastname"];
                     $model->adminUpdate($column,$data,$id);
@@ -61,16 +51,12 @@ class UpdateController extends LayoutController
                 }
                 else
                 {
-                    echo("ERREUR 3");
-                    echo("<br>");
                     $errors["lastname"] = "Veuillez renseigner votre Nom avant de valider.";
                 }
                 break;
             case isset($_POST["firstname"]):
                 if(!empty($_POST["firstname"]))
                 {
-                    echo("OK 4");
-                    echo("<br>");
                     $column = "adm_first_name";
                     $data = $_POST["firstname"];
                     $model->adminUpdate($column,$data,$id);
@@ -78,19 +64,12 @@ class UpdateController extends LayoutController
                 }
                 else
                 {
-                    echo("ERREUR 4");
-                    echo("<br>");
                     $errors["firstname"] = "Veuillez renseigner votre Prénom avant de valider.";
                 }
                 break;
         }
         if(isset($errors) && !empty($errors))
         {
-            echo("AFFICHE LES ERREURS");
-            echo("<br>");
-            echo("<pre>");
-            print_r($errors);
-            echo("</pre>");
             $admin = $model->adminInfo();
             $this->render("administrator",["admin"=>$admin,"errors"=>$errors]);
         }
