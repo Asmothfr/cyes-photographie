896c987cb356f023a0f6d9838ac8fdd24036ba62
Asmothfr <yoan.sabatier@gmail.com>
2022-08-31 18:58:51 +0200

    css

--- 654381dfhgf1871t2gdf03/Database/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "cyes_photographie");
-define("DB_USER" , "root");
-define("DB_PASSWORD" , "");
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 use Library\Router;
 
 spl_autoload_register(function ($className) {
-    require lcfirst(str_replace('\\', '/', $className)) . '.php';
+    require (str_replace('\\', '/', $className)) . '.php';
 });
 
 $router = new Router();
