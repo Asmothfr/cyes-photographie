b1a48c0889d822d498fd97b9a80c1db39df59a7e
Asmothfr <yoan.sabatier@gmail.com>
2022-10-04 12:55:40 +0200

    js translation and about delete patch

--- 654381dfhgf1871t2gdf03/App/Controllers/DeleteController.php
+++ 654381dfhgf1871t2gdf03/App/Controllers/DeleteController.php
@@ -112,28 +112,28 @@ class DeleteController extends LayoutController
                     case(isset($_GET["abt_photo"]) && !empty($_GET["abt_photo"])):
                         $phtName = $_GET["abt_photo"];
                         $column = "abt_photo";
-                        $data = ["newData"=>null];
+                        $data = ["newData"=>""];
                         $model->deleteAboutContent($column,$data);
                         unlink("../assets/img/about_img/$phtName");
                         break;
                     case(isset($_GET["abt_content"]) && !empty($_GET["abt_content"])):
                         $column = "abt_content";
-                        $data = ["newData"=>null];
+                        $data = ["newData"=>""];
                         $model->deleteAboutContent($column,$data);
                         break;
                     case(isset($_GET["abt_facebook"]) && !empty($_GET["abt_facebook"])):
                         $column = "abt_facebook";
-                        $data = ["newData"=>null];
+                        $data = ["newData"=>""];
                         $model->deleteAboutContent($column,$data);
                         break;
                     case(isset($_GET["abt_instagram"]) && !empty($_GET["abt_instagram"])):
                         $column = "abt_instagram";
-                        $data = ["newData"=>null];
+                        $data = ["newData"=>""];
                         $model->deleteAboutContent($column,$data);
                         break;
                     case(isset($_GET["abt_twitter"]) && !empty($_GET["abt_twitter"])):
                         $column = "abt_twitter";
-                        $data = ["newData"=>null];
+                        $data = ["newData"=>""];
                         $model->deleteAboutContent($column,$data);
                         break;
                 }
--- assets/js/nav_translation.js
+++ assets/js/nav_translation.js
@@ -3,22 +3,41 @@ window.addEventListener("DOMContentLoaded", () => {
     let url = document.location.href;
     let baseUrl = url.includes("cyes-photographie/654381dfhgf1871t2gdf03");
    
-    function homeFrontOffice(){
-        let urlEnd = url.substring (url.lastIndexOf( "/" )+1);
-        const isHome = urlEnd === "index.php?route=home"
+    function homeFrontOffice()
+    {
+        let urlEnd = url.substring (url.lastIndexOf( "/" )+10);
         const title = document.querySelector(".nav__title");
-        title.classList.toggle("nav__title--home", isHome)
-        title.classList.toggle("nav__title--page", !isHome)
+        switch(urlEnd)
+        {
+            case "?route=home":
+                title.classList.toggle("nav__title--page")
+                break
+            case "":
+                title.classList.toggle("nav__title--page")
+                break
+            default:
+                title.classList.toggle("nav__title--home")
+                break
+        }
     }
     function homeBackOffice()
     {
-        let urlEnd = url.substring (url.lastIndexOf( "/" )+1);
-        const isHome = urlEnd === "index.php?route=home"
+        let urlEnd = url.substring (url.lastIndexOf( "/" )+10);
         const title = document.querySelector(".nav__title-admin");
         const nav = document.querySelector(".nav__list");
-        nav.classList.toggle("nav__list--homeAdmin", isHome)
-        title.classList.toggle("nav__title-admin--home", isHome)
-        title.classList.toggle("nav__title--page", !isHome)
+        switch(urlEnd)
+        {
+            case "?route=home":
+                title.classList.toggle("nav__title--page")
+                break
+            case "":
+                title.classList.toggle("nav__title--page")
+                break
+            default:
+                nav.classList.toggle("nav__list--homeAdmin")
+                title.classList.toggle("nav__title-admin--home")
+                break
+        }
     }
     if(baseUrl)
     {
